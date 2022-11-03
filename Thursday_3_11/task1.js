316259b7980d4534a73d6f5e09ed2dfc36b152b5
Obaida Al-thunibat <obaidaalthunibat@gmail.com>
2022-11-03 16:23:05 +0200

    change

---
+++ Thursday_3_11/task1.js
@@ -0,0 +1,17 @@
+let x = prompt('x');
+let y = prompt('y');
+
+
+if(x > y)
+{
+   console.log('Hello World') 
+   alert('Hello World')
+   document.write('Hello World')
+}
+else{
+    console.log('Goodbye ') 
+    alert('Goodbye')
+    document.write('Goodbye')
+}
+
+

6e883069d524f69e9325dda82f0aedc883ff4925
Obaida Al-thunibat <obaidaalthunibat@gmail.com>
2022-11-15 01:01:13 +0300

    change

---
+++ Sunday_13_11/for of loop.js
@@ -0,0 +1,18 @@
+// const Ourgroup = ["salwa ", "bayan ", "mohammed " , "obaida " , "mohammed "];
+const Ourgroup = [1, 2, 3, 4, 5];
+
+let text = "";
+for (let x of Ourgroup) {
+  text += x 
+console.log(text)
+}
+
+// ------------------------------------------------------------------------------------
+
+// for (i=0;i<=2;i++) {
+//   console.log(Ourgroup[i])
+// }
+
+// for (i=0; i<Ourgroup.length; i++) {
+//   console.log(Ourgroup[i])
+// }
